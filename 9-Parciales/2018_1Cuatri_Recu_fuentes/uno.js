fab2b475afaf03c9c45152e99b7d4ddd4bbd15aa
andrescimma1 <andrescimma1@gmail.com>
2019-02-14 19:50:06 -0300

    1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var mascota1, mascota2, peso1, peso2, suma;
 
+	mascota1 = prompt("Ingrese el nombre de su mascota");
+	mascota2 = prompt("Ingrese el nombre otra mascota");
+	peso1 = prompt("Ingrese el peso de " + mascota1);
+	peso2 = prompt("Ingrese el peso de " + mascota2);
+
+	peso1 = parseInt(peso1);
+	peso2 = parseInt(peso2);
+
+	suma = peso1 + peso2;
+
+    alert ("tenes dos mascotas " + mascota1 + " y " + mascota2 + ", que pesan " + peso1 + " y " + peso2 + " kilos, la suma de los kilos es " + suma);
 }
