be97a145bdce0a8fba2017c49d88b8059fc3e4f3
andrescimma1 <andrescimma1@gmail.com>
2019-02-14 20:20:12 -0300

    2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
+   var compra, desc, iva, resultado;
 
+   compra = elPrecioFinal.value;
+
+   compra = parseInt(compra);
+   desc = parseInt(desc);
+   iva = parseInt(iva);
+
+   desc = compra * 0.90;
+   iva = desc * 0.21;
+   resultado = desc + iva;
+
+   alert("tu compra es de $" + compra + " tenes un descuento del 10% queda en $" + desc + ", mas el iva es $" + resultado);
 }
+
