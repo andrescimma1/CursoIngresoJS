e7474967d574cd5ce48159e7993145bee38731ce
andrescimma1 <andrescimma1@gmail.com>
2019-02-21 21:21:56 -0300

    Ejercicio if 4 no da bien el resultado del aumento

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,11 @@
-function mostrar()
-{
+/*
+2- en cinco.js solo switch
+		cada habitación de un hotel tiene un precio, hay promociones según el tipo de pago,
+	si es con tarjeta visa un 10%, si es por paypal un 15%, por mercadoPago un 10%, efectivo
+	
 
-}
+
+
+
+
+*/
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -4,7 +4,7 @@ function mostrar()
 		En un restaurante, un grupo de amigos quiere saber cuánto debe pagar cada uno del total, recordar que se 
 		debe agregar el 10% de propina, y que los precios no incluyen el iva(21%), mostrar toda la información
 		 en un solo alert*/
-
+/*
 	var grupo, precio, propina, total, repartir;
 
 	grupo = prompt("¿Cuantos son en el grupo de amigos?");
@@ -21,5 +21,121 @@ function mostrar()
 	repartir = total / grupo;
 
 	alert("Ustedes son " + grupo + ", si el importe a pagar es $" + precio + " y sumandole la propina son $" + total + ". Cada uno tiene que pagar $" + repartir);
- 
+*/
+
+	var a, b, c, d, mensaje, descuento, importemod, resultado, porcentaje;
+
+	a = prompt("Ingrese el primer valor");
+	b = prompt("Ingrese el segundo valor");
+	c = prompt("Ingrese el tercer valor");
+	d = prompt("Ingrese el cuarto valor");	
+
+	a = parseInt(a);
+	b = parseInt(b);
+	c = parseInt(c);
+	d = parseInt(d);
+	descuento = parseInt(descuento);
+	importemod = parseInt(importemod);
+	resultado = parseInt(resultado);
+
+
+
+	if(a > b && a > c && a > d)
+	{	
+		if(a > 100)
+		{
+			porcentaje = 10;
+			importemod = a;
+		}
+		else
+		{
+			if(a > 50)
+			{
+				porcentaje = 5;
+				importemod = a;
+			}
+			else
+			{
+				porcentaje = 115;
+				importemod = a;
+			}
+		}
+	}
+	else
+	{
+		if(b > a && b > c && b > d)
+		{
+			if(b > 100)
+			{
+				porcentaje = 10;
+				importemod = b;
+			}
+			else
+			{
+			if(b > 50)
+			{
+				porcentaje = 5;
+				importemod = b;
+			}
+			else
+			{
+				porcentaje = 115;
+				importemod = b;
+			}
+			}
+		}
+		else
+		{
+		if(c > a && c > b && c > d)
+		{
+			if(c > 100)
+			{
+				porcentaje = 10;
+				importemod = c;
+			}
+			else
+			{
+			if(c > 50)
+			{
+				porcentaje = 5;
+				importemod = c;
+			}
+			else
+			{
+				porcentaje = 115;
+				importemod = c;
+			}
+			}
+		}
+		else
+		{
+			if(d > a && d > b && d > c)
+			{
+				if(d > 100)
+				{
+					porcentaje = 10;
+					importemod = d;
+				}
+				else
+				{
+				if(d > 50)
+				{
+					porcentaje = 5;
+					importemod = d;
+				}
+				else
+				{
+					porcentaje = 115;
+					importemod = d;
+				}
+				}
+			}
+		}
+
+	descuento = importemod * porcentaje / 100;
+	resultado = importemod - descuento;
+	mensaje = "Su importe mayor es: " + importemod + ". El importe modificado es: " + resultado;
+	alert(mensaje); 
+}
+}
 }
