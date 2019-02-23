9aeea67f7fb6a4941f84c4d3fbfba960054ddf51
Andy <andydile@gmail.com>
2019-02-23 20:49:52 -0300

    TP RisingBTL terminado

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,96 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var sueldo;
+	var legajo;
+	var nacionalidad;
+
+	//Validando la edad.
+	edad = prompt("Ingrese la edad entre 18 a 90 años inclusive");
+ 	while(edad < 18 || edad > 90)
+ 	{
+ 		edad = prompt("Error, ingrese la edad entre 18 a 90 años inclusive");
+ 	}
+ 	document.getElementById('Edad').value=edad;
+
+ 	//Validando el sexo.
+ 	sexo = prompt("Ingrese el sexo, “M” para masculino y “F” para femenino");
+ 	while(sexo != "F" && sexo != "M")
+ 	{
+ 		sexo = prompt("Error, ingrese el sexo, “M” para masculino y “F” para femenino");
+ 	}
+ 	document.getElementById('Sexo').value=sexo;
+
+ 	//Validando el estado civil.
+ 	estadoCivil = prompt("Ingrese el estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+ 	while(estadoCivil < 1 || estadoCivil > 4)
+ 	{
+ 		estadoCivil = prompt("Error, ingrese el estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+ 	}
+ 	switch(estadoCivil)
+ 	{
+ 		case "1":
+ 			estadoCivil = "Soltero";
+ 			estadoCivil = document.getElementById('EstadoCivil').value=estadoCivil;
+ 			break;
+ 		case "2":
+ 			estadoCivil = "Casado";
+ 			estadoCivil = document.getElementById('EstadoCivil').value=estadoCivil;
+ 			break;
+ 		case "3":
+ 			estadoCivil = "Divorciado";
+ 			estadoCivil = document.getElementById('EstadoCivil').value=estadoCivil;
+ 			break;
+ 		case "4":
+ 			estadoCivil = "Viudo";
+ 			estadoCivil = document.getElementById('EstadoCivil').value=estadoCivil;
+ 			break;
+ 		default:
+ 			break;
+ 	}
+
+ 	//Validando el sueldo bruto.
+ 	sueldo = prompt("Ingrese su sueldo bruto, no menor a 8000.");
+ 	while(sueldo < 8000)
+ 	{
+ 		sueldo = prompt("Error, ingrese su sueldo bruto, no menor a 8000.");
+ 	}
+ 	document.getElementById('Sueldo').value=sueldo;
+
+ 	//Validando el legajo
+ 	legajo = prompt("Ingrese el numero de legajo, numérico de 4 cifras, sin ceros a la izquierda");
+ 	while(legajo < 1000 || legajo > 9999)
+ 	{
+ 		legajo = prompt("Error, ingrese el numero de legajo, numérico de 4 cifras, sin ceros a la izquierda");
+ 	}
+ 	document.getElementById('Legajo').value=legajo;
+
+ 	//Validando nacionalidad
+ 	nacionalidad = prompt("Ingrese nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+ 	while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N")
+ 	{
+ 		nacionalidad = prompt("Error, ingrese nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+ 	}
+ 	switch(nacionalidad)
+ 	{
+ 		case "A":
+ 			nacionalidad = "Argentino";
+ 			nacionalidad = document.getElementById('Nacionalidad').value=nacionalidad;
+ 			break;
+ 		case "E":
+ 			nacionalidad = "Extranjero";
+ 			nacionalidad = document.getElementById('Nacionalidad').value=nacionalidad;
+ 			break;
+ 		case "N":
+ 			nacionalidad = "Nacionalizado";
+ 			nacionalidad = document.getElementById('Nacionalidad').value=nacionalidad;
+ 			break;
+ 		default:
+ 			break;
+ 	}
+
+
 }
