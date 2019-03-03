4580f06a6f67df82d41b2957ac65063e156129a1
Andy <andydile@gmail.com>
2019-03-03 17:48:08 -0300

    Ejercicio 3 del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var precioFinal;
+	var precioFinalConDescuento;
 
+	precio = prompt("Ingrese el precio");
+	porcentaje = prompt("Ingrese el porcentaje de descuento");
+
+	precio = parseInt(precio);
+	porcentaje = parseInt(porcentaje);
+
+	precioFinal = precio * porcentaje / 100;
+	precioFinalConDescuento = precio - precioFinal;
+
+	document.getElementById('elPrecioFinal').value=precioFinalConDescuento;
 }
