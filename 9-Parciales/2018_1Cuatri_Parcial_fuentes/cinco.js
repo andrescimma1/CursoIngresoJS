fa8199022a64ced8d4f0f4ed46abd9abd9876da8
Andy <andydile@gmail.com>
2019-03-03 18:30:02 -0300

    Ejercicio 5 del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var sistemaSolar;
 
+	sistemaSolar = prompt("Ingrese un planeta");
+
+
+	switch(sistemaSolar)
+	{
+		case "tierra":
+			mensaje = "acá vivimos";
+			break;
+		case "mercurio":
+			mensaje = "acá hace más calor";
+			break;
+		case "venus":
+			mensaje = "acá hace más calor";
+			break;
+		default:
+			mensaje = "acá hace más frio";
+			break;
+	}
+
+	alert(mensaje);
 }
