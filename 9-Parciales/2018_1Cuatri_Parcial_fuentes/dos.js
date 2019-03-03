307083a771e595b8f8e8c76894f816b3aee72513
Andy <andydile@gmail.com>
2019-03-03 17:33:38 -0300

    Ejercicio 2 del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var Nombre;
+	var Localidad;
+
+	Nombre = elNombre.value;
+	Localidad = laLocalidad.value;
+
+	alert("usted es " + Nombre + " y vive en la localidad de " + Localidad);
+
+
   
 }
