261f58b90d9ba468801e553205d25c6eabc7e7b4
Andy <andydile@gmail.com>
2019-03-03 18:09:45 -0300

    Ejercicio 4 del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,35 @@
 function mostrar()
 {
+	var numero1;
+	var numero2;
+	var resta;
+	var suma;
+
+	numero1 = prompt("Ingrese el primer numero");
+	numero2 = prompt("Ingrese el segundo numero");
+
+	if(numero1 == numero2)
+	{
+		alert(numero1 + numero2);
+	}
+
+	numero1 = parseInt(numero1);
+	numero2 = parseInt(numero2);
+
+	if(numero1 > numero2)
+	{
+		resta = numero1 - numero2;
+		alert(resta);
+	}
+	else
+	{
+		suma = numero1 + numero2;
+		alert(suma);
+	}
+
+	if(suma > 10)
+	{
+		alert("La suma es " + suma + " y supero el 10");
+	}
 
 }
