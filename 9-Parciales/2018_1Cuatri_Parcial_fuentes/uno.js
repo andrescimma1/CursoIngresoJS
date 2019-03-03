68a82f999d21f80457dd22416ce4b8e8436ca410
Andy <andydile@gmail.com>
2019-03-03 17:28:11 -0300

    Ejercicio 1 del parcial terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho = prompt("Ingrese el ancho de un rectangulo");
+	largo = prompt("Ingrese el largo de un rectangulo");
+
+	ancho = parseInt(ancho);
+	largo = parseInt(largo);
+
+	perimetro = 2*(ancho + largo);
+
+	alert("El perimetro es " + perimetro);
+
 
 }
