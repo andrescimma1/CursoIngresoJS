a0c22019db7066921ad54588cee35f3f25bb6a0a
Andy <andydile@gmail.com>
2019-03-03 15:46:59 -0300

    Ejercicio 8 for terminado

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,30 @@
 function mostrar()
 {
+	var numeroIngresado;
+	var numeroAnterior;
+
+	numeroIngresado = prompt("Ingrese un numero");
+	numeroIngresado = parseInt(numeroIngresado);
+
+	for(numeroAnterior = numeroIngresado - 1; numeroAnterior > 1; numeroAnterior--)
+	{
+
+		console.log(numeroAnterior);
+		if(numeroIngresado%numeroAnterior == 0)
+		{
+			break;
+		}
+
+	}
+
+	if(numeroAnterior == 1)
+	{
+		alert("Es primo");
+	}
+	else
+	{
+		alert("No es primo");
+	}
 
 
-
-
-}//FIN DE LA FUNCIÓN
+}
