ed3dd7228962e125aa54765673541f59657fa850
Andy <andydile@gmail.com>
2019-03-03 15:38:02 -0300

    Ejercicio 7 terminado

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,29 @@
 function mostrar()
 {
+	var numero;
+	var numeroIngresado;
+	var contador = 0;
 
+	numero = prompt("Ingrese un numero");
+	numero = parseInt(numero);
 
+	for(numeroIngresado = 0; numero > 1; )
+	{
+		numeroIngresado = numeroIngresado + 1;
+		if(numero%numeroIngresado == 0)
+		{
+			document.write(numeroIngresado + "</br>");
+			contador = contador + 1;
+		}
+
+		if(numeroIngresado == numero)
+		{
+			break;
+		}
+
+	}
+
+	document.write("La cantidad de numeros divisores encontrados es: " + contador);
 
 
 }//FIN DE LA FUNCIÓN
