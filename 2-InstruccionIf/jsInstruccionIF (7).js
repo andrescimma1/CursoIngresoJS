2efac7cd47140a88e337bcb616ab4de06c3d98b0
Andy <andydile@gmail.com>
2019-03-03 16:00:54 -0300

    Ejercicio 7 if terminado

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+	var Edad;
+	var EstadoCivil;
 
-	
+	Edad = edad.value;
+	EstadoCivil = estadoCivil.value;
 
+	if(Edad < 18 && EstadoCivil != "Soltero")
+	{
+		alert("Es muy pequeño para NO ser soltero.");
+	}
 
 }//FIN DE LA FUNCIÓN
