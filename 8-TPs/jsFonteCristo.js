30625020fe8eaad56f542d665554bc258e9e4cba
Andy <andydile@gmail.com>
2019-03-05 20:57:19 -0300

    TP FonteCristo terminado

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -7,7 +7,129 @@ c.	Se pedirán un número positivo y se mostrará la cantidad de números divisi
 d.	Se pedirán un número positivo y se mostrará si el número es un número primo o no.
 e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos desde el número ingresado hasta el cero.
 */
-function ComenzarIngreso () 
+function NumerosPares(){
+
+ 	var numeroPositivo = 0;
+ 	var numeroAnterior = 0;
+
+ 	numeroPositivo = numero.value;
+
+
+ 	while(numeroPositivo < 0)
+ 	{
+ 		numeroPositivo = prompt("Error, ingrese un numero positivo");
+ 	}
+ 	numeroPositivo = parseInt(numeroPositivo);
+
+
+
+ 	for(numeroAnterior = numeroPositivo ; numeroAnterior > -1 ;)
+ 	{
+ 		if(numeroAnterior%2 == 0)
+ 		{
+ 			alert(numeroAnterior);
+ 		}
+ 		numeroAnterior = numeroAnterior - 1;
+ 	}
+
+}
+
+function NumerosImpares()
 {
+	numeroPositivo = numero.value;
+
+	while(numeroPositivo < 0)
+ 	{
+ 		numeroPositivo = prompt("Error, ingrese un numero positivo");
+ 	}
+ 	numeroPositivo = parseInt(numeroPositivo);
+
+
+
+ 	for(numeroAnterior = numeroPositivo ; numeroAnterior > -1 ;)
+ 	{
+ 		if(numeroAnterior%2 == 1)
+ 		{
+ 			alert(numeroAnterior);
+ 		}
+ 		numeroAnterior = numeroAnterior - 1;
+ 	}
+
+} 
+
+function NumerosDivisibles(){
+
+	numeroPositivo = numero.value;
+
+	while(numeroPositivo < 0)
+ 	{
+ 		numeroPositivo = prompt("Error, ingrese un numero positivo");
+ 	}
+ 	numeroPositivo = parseInt(numeroPositivo);	
+
+ 	for(numeroAnterior = numeroPositivo ; numeroAnterior > -1 ;)
+ 	{
+ 		if(numeroPositivo%numeroAnterior == 0)
+ 		{
+ 			alert(numeroAnterior);
+ 		}
+ 		numeroAnterior = numeroAnterior - 1;
+ 	}
+
+}
+
+function VerificarPrimo(){
+
+	numeroPositivo = numero.value;
+
+	while(numeroPositivo < 0)
+ 	{
+ 		numeroPositivo = prompt("Error, ingrese un numero positivo");
+ 	}
+ 	numeroPositivo = parseInt(numeroPositivo);	
+
+ 	for(numeroAnterior = numeroPositivo - 1; numeroAnterior > 1; numeroAnterior-- )
+ 	{
+ 		if(numeroPositivo%numeroAnterior == 0)
+ 		{
+ 			break;
+ 		}
+
+ 	}
+
+ 	if(numeroAnterior == 1)
+ 	{
+ 		alert("Es primo");
+ 	}
+ 	else
+ 	{
+ 		alert("No es primo");
+ 	}
+
+}
+
+function NumerosPrimos(){
+
+	var contador = 0;
+
+	numeroPositivo = numero.value;
+
+	while(numeroPositivo < 0)
+ 	{
+ 		numeroPositivo = prompt("Error, ingrese un numero positivo");
+ 	}
+ 	numeroPositivo = parseInt(numeroPositivo);	
+
+ 	for(numeroAnterior = numeroPositivo - 1; numeroAnterior > 1; numeroAnterior-- )
+ 	{
+ 		if(numeroPositivo%numeroAnterior == 0)
+ 		{
+ 			contador = contador + 1;
+ 		}
+
+ 	}
  	
+ 	alert(contador + " numeros primos");
+
+
 }
